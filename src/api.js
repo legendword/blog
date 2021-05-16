e368fdd7819af0d4325885d810e0335495357f73
Legendword <zwh@legendword.com>
2021-05-16 13:15:55 -0700

    using @capacitor-community/http for capacitor api requests
    - this is an attempt to fix session cookies not saving in capacitor due to MKWebView restrictions.
    - still testing new capacitor http api, argument error on login requests needs fixing

--- src/api.js
+++ src/api.js
@@ -1,18 +1,50 @@
 import axios from 'axios'
+import { Platform } from 'quasar'
+import '@capacitor-community/http'
+import { Plugins } from '@capacitor/core'
 
-axios.defaults.withCredentials = true
-const axiosInstance = axios.create({
-    withCredentials: true,
-    baseURL: 'https://analycube.com:8001'
-})
+const baseURL = 'https://analycube.com:8001'
 
-//const base = 'https://legendword.com/wordblog'
-
-const api = {
-    //get: (path, params) => axiosInstance.get(`${path}`, { params: params, headers: {'X-Client-Version': '0.2'} }),
-    get: (path, params) => axiosInstance.get(`${path}`, { params: params }),
-    post: (path, params) => axiosInstance.post(`${path}`, params),
-    put: (path, params) => axiosInstance.put(`${path}`, params),
-    delete: (path, params) => axiosInstance.delete(`${path}`, params)
+var api;
+if (Platform.is.capacitor) {
+    const { Http } = Plugins
+    api = {
+        get: (path, params) => Promise.resolve(Http.request({
+            method: 'GET',
+            url: baseURL + path,
+            params
+        })),
+        post: (path, params) => Promise.resolve(Http.request({
+            method: 'POST',
+            url: baseURL + path,
+            data: params
+        })),
+        put: (path, params) => Promise.resolve(Http.request({
+            method: 'PUT',
+            url: baseURL + path,
+            data: params
+        })),
+        delete: (path, params) => Promise.resolve(Http.request({
+            method: 'DELETE',
+            url: baseURL + path,
+            data: params
+        }))
+    }
 }
+else {
+    axios.defaults.withCredentials = true
+    const axiosInstance = axios.create({
+        withCredentials: true,
+        baseURL: baseURL
+    })
+
+    api = {
+        //get: (path, params) => axiosInstance.get(`${path}`, { params: params, headers: {'X-Client-Version': '0.2'} }),
+        get: (path, params) => axiosInstance.get(`${path}`, { params: params }),
+        post: (path, params) => axiosInstance.post(`${path}`, params),
+        put: (path, params) => axiosInstance.put(`${path}`, params),
+        delete: (path, params) => axiosInstance.delete(`${path}`, params)
+    }
+}
+
 export default api;
